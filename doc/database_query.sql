94ad0ef0f6efdb62846c1498f7303281849d3870
Hasbi Alwi Kusmana <hasbialwi70@gmail.com>
2023-12-24 21:50:36 +0700

    documentasi database query

---
+++ doc/database_query.sql
@@ -0,0 +1,16 @@
+CREATE TABLE users (
+    id VARCHAR PRIMARY KEY,
+    name VARCHAR NOT NULL,
+    email VARCHAR NOT NULL,
+    password VARCHAR NOT NULL,
+    phone VARCHAR NOT NULL,
+    company VARCHAR,
+    position VARCHAR,
+    description VARCHAR,
+    photo VARCHAR,
+    role VARCHAR NOT NULL,
+    instagram VARCHAR,
+    github VARCHAR,
+    linkedin VARCHAR,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+);
