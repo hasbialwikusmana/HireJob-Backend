081bdfe7dee59d3752af177fd47f12898bda270e
Hasbi Alwi Kusmana <hasbialwi70@gmail.com>
2023-12-28 23:40:09 +0700

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const router = express.Router();
-const workerRouter = require("./worker");
-const recruiterRouter = require("./recruiter");
+const usersRouter = require("./users");
+// const workerRouter = require("./worker");
+// const recruiterRouter = require("./recruiter");
+// const skillRouter = require("./skill");
+// const portfolioController = require("./portfolio");
+// const workExperienceController = require("./workExperience");
 
-router.use("/worker", workerRouter);
-router.use("/recruiter", recruiterRouter);
+router.use("/", usersRouter);
+// router.use("/worker", workerRouter);
+// router.use("/recruiter", recruiterRouter);
+// router.use("/skill", skillRouter);
+// router.use("/portfolio", portfolioController);
+// router.use("/work-experience", workExperienceController);
 
 module.exports = router;
