5427bf271c5d230eff5cf4ab5a54ea58b0400087
Hasbi Alwi Kusmana <hasbialwi70@gmail.com>
2023-12-29 03:54:35 +0700

    Create workExperience.js

---
+++ src/routes/workExperience.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const router = express.Router();
+
+const { protect, isWorker } = require("../middlewares/auth");
+
+const { getAllWorkExperiences, getDetailWorkExperience, createWorkExperience, updateWorkExperience, deleteWorkExperience } = require("../controllers/workExperience");
+
+//WorkExperiences router
+router.get("/", getAllWorkExperiences);
+router.get("/:id", getDetailWorkExperience);
+router.post("/", protect, isWorker, createWorkExperience);
+router.put("/:id", protect, isWorker, updateWorkExperience);
+router.delete("/:id", protect, isWorker, deleteWorkExperience);
+
+module.exports = router;
