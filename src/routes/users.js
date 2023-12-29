5931859eab3e47ba23154c0f98a003d852cc9a50
Hasbi Alwi Kusmana <hasbialwi70@gmail.com>
2023-12-29 09:39:12 +0700

    Update users.js

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,21 +1,23 @@
 const express = require("express");
 const router = express.Router();
 const usersControllers = require("../controllers/users");
-const { protect } = require("../middlewares/auth");
-const { upload } = require("../middlewares/upload");
+const { protect, isRecruiter, isWorker } = require("../middlewares/auth");
+const { upload, uploadBanner } = require("../middlewares/upload");
 
 router.post("/register", usersControllers.register);
 router.post("/login", usersControllers.login);
 
-// router.get("/", workerController.getAllWorker);
-router.get("/profile", protect, usersControllers.getProfile);
+// router.get("/",protect,isRecruiter, workerController.getAllWorker);
+router.get("/profile", protect, isRecruiter, isWorker, usersControllers.getProfile);
 
-router.get("/profile/:id", protect, usersControllers.getProfileById);
+router.get("/profile/:id", protect, isRecruiter, isWorker, usersControllers.getProfileById);
 
-router.put("/profile/update-profile/:id", protect, usersControllers.updateProfile);
+router.put("/profile/update-profile/:id", protect, isRecruiter, isWorker, usersControllers.updateProfile);
 
-router.put("/profile/update-image/:id", protect, upload, usersControllers.updateImageProfile);
+router.put("/profile/update-image/:id", protect, isRecruiter, isWorker, upload, usersControllers.updateImageProfile);
 
-// router.delete("/profile/delete", protect, workerController.deleteWorkers);
+router.put("/profile/update-banner/:id", protect, isRecruiter, uploadBanner, usersControllers.updateBannerProfile);
+
+router.delete("/profile/delete/:id", protect, isRecruiter, isWorker, usersControllers.deleteUsers);
 
 module.exports = router;
