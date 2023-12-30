c50b826704b55f670c7228e84e328580d22d8084
Hasbi Alwi Kusmana <hasbialwi70@gmail.com>
2023-12-30 20:11:23 +0700

    Update recruiter.js

--- src/controllers/recruiter.js
+++ src/controllers/recruiter.js
@@ -1,148 +1,61 @@
 const commonHelper = require("../helpers/common");
-const authHelper = require("../helpers/auth");
-const bcrypt = require("bcryptjs");
-const createError = require("http-errors");
-const errorServ = new createError.InternalServerError();
-const { v4: uuidv4 } = require("uuid");
-const jwt = require("jsonwebtoken");
 const cloudinary = require("../middlewares/cloudinary");
 
-const { findEmail, selectAllRecruiters, createUser, updatePhotoUser, updateBanner, updaterecruiter, deleteRecruiter } = require("../models/recruiter");
+const { findEmail, findId, deleteUsersAccount } = require("../models/users");
+const { selectAllRecruiter, selectProfileRecruiter, updateRecruiter, updateImageRecruiter, updateBannerRecruiter } = require("../models/recruiter");
 
-const recruiterController = {
+const recruiterControllers = {
   getAllRecruiter: async (req, res) => {
     try {
-      const { rows } = await selectAllRecruiters();
-      commonHelper.response(res, rows, 200);
+      const filter = req.query.filter || "name";
+      const searchQuery = req.query.search || "";
+      const sortBy = req.query.sortBy || "name";
+      const sort = req.query.sort || "asc";
+      const limit = Number(req.query.limit) || 5;
+      const page = Number(req.query.page) || 1;
+      const offset = (page - 1) * limit;
+      const results = await selectAllRecruiter(filter, searchQuery, sortBy, sort, limit, offset);
+
+      if (!results.rowCount) return commonHelper.response(res, null, 404, "Recruiters not found");
+
+      const totalData = Number(results.rowCount);
+      const totalPage = Math.ceil(totalData / limit);
+      const pagination = {
+        currentPage: page,
+        limit: limit,
+        totalData: totalData,
+        totalPage: totalPage,
+      };
+      commonHelper.response(res, results.rows, 200, "Get all recruiters successful", pagination);
     } catch (error) {
       console.log(error);
+      commonHelper.response(res, null, 500, "Failed getting recruiters");
     }
   },
-
-  registerRecruiter: async (req, res) => {
+  getRecruiterById: async (req, res) => {
     try {
-      const { name, email, company_name, jobdesk, nohp, password, confirmPassword, role } = req.body;
+      const id = req.params.id;
 
-      // Check if email already exists
-      const { rowCount } = await findEmail(email);
-      if (rowCount) {
-        return res.json({
-          Message: "Email is already exist",
-        });
-      }
+      const result = await selectProfileRecruiter(id);
 
-      // Check if the passwords match
-      if (password !== confirmPassword) {
-        return res.status(400).json({
-          Message: "Password and confirmPassword do not match",
-        });
-      }
+      if (!result.rowCount) return commonHelper.response(res, null, 404, "Recruiter not found");
 
-      // Check if the role is 'recruiter'
-      if (role !== "recruiters") {
-        return res.status(400).json({
-          Message: "Invalid role. Only 'recruiters' is allowed",
-        });
-      }
+      commonHelper.response(res, result.rows, 200, "Get detail recruiter successful");
+    } catch (error) {
+      console.log(error);
+      commonHelper.response(res, null, 500, "Failed getting detail Recruiter");
+    }
+  },
+  updateProfileRecruiter: async (req, res) => {
+    try {
+      const id = req.params.id;
 
-      const salt = bcrypt.genSaltSync(10);
-      const passwordHash = bcrypt.hashSync(password, salt);
-      const id = uuidv4();
+      const { name, email, company_name, jobdesk, nohp, company_field, workplace, description, instagram, linkedin } = req.body;
 
-      let data = {
+      const data = {
         id,
         name,
         email,
-        company_name,
-        jobdesk,
-        nohp,
-        password: passwordHash,
-        role,
-      };
-
-      createUser(data)
-        .then((result) => commonHelper.response(res, result.rows, 201, "Register success"))
-        .catch((err) => res.send(err));
-    } catch (error) {
-      console.log(error);
-    }
-  },
-
-  login: async (req, res, next) => {
-    try {
-      const { email, password, role } = req.body;
-      const {
-        rows: [recruiter],
-      } = await findEmail(email);
-      if (!recruiter) {
-        return res.status(403).json({
-          Message: "Email is invalid",
-        });
-      }
-      const isValidPassword = bcrypt.compareSync(password, recruiter.password);
-
-      if (!isValidPassword) {
-        return res.status(403).json({
-          Message: "Password is invalid",
-        });
-      }
-      delete recruiter.password;
-      const payload = {
-        email: recruiter.email,
-        id: recruiter.id,
-        role: recruiter.role,
-      };
-      recruiter.token = authHelper.generateToken(payload);
-      recruiter.refreshToken = authHelper.generateRefreshToken(payload);
-
-      if (role !== recruiter.role) {
-        return res.status(403).json({
-          Message: "Role is invalid",
-        });
-      }
-
-      commonHelper.response(res, recruiter, 201, "login is successful");
-    } catch (error) {
-      console.log(error);
-      next(errorServ);
-    }
-  },
-  refreshToken: (req, res) => {
-    const refreshToken = req.body.refreshToken;
-    const decoded = jwt.verify(refreshToken, process.env.SECRET_KEY_JWT);
-    const payload = {
-      email: decoded.email,
-      id: decoded.id,
-    };
-    const result = {
-      token: authHelper.generateToken(payload),
-      refreshToken: authHelper.generateRefreshToken(payload),
-    };
-    commonHelper.response(res, result, 200, "Refresh token success");
-  },
-
-  profileRecruiter: async (req, res) => {
-    const email = req.payload.email;
-
-    const {
-      rows: [recruiter],
-    } = await findEmail(email);
-    delete recruiter.password;
-    commonHelper.response(res, recruiter, 200);
-  },
-
-  updateUserRecruiter: async (req, res) => {
-    try {
-      const email = req.payload.email;
-
-      const {
-        rows: [recruiter],
-      } = await findEmail(email);
-      const { name, company_name, jobdesk, nohp, company_field, workplace, description, instagram, linkedin } = req.body;
-
-      const data = {
-        id: recruiter.id,
-        name,
         company_name,
         jobdesk,
         nohp,
@@ -152,8 +65,8 @@ const recruiterController = {
         instagram,
         linkedin,
       };
-      console.log(data);
-      updaterecruiter(data)
+
+      updateRecruiter(data)
         .then((result) => {
           commonHelper.response(res, result.rows, 200, "Recruiter updated");
         })
@@ -163,57 +76,57 @@ const recruiterController = {
     }
   },
 
-  updatePhotoRecruiter: async (req, res) => {
+  updateImageProfile: async (req, res) => {
     try {
-      const email = req.payload.email;
+      const id = req.params.id;
 
       const result = await cloudinary.uploader.upload(req.file.path);
       const image = result.secure_url;
 
       const {
-        rows: [recruiter],
-      } = await findEmail(email);
+        rows: [users],
+      } = await findId(id);
 
       const data = {
-        id: recruiter.id,
+        id: users.id,
         image,
       };
 
-      if (recruiter.image !== null) {
-        const public_id = recruiter.image.split("/").pop().split(".").shift();
+      if (users.image !== null) {
+        const public_id = users.image.split("/").pop().split(".").shift();
         await cloudinary.uploader.destroy(public_id);
       }
 
-      updatePhotoUser(data)
-        .then((result) => commonHelper.response(res, result.rows, 200, "Update photo success"))
+      updateImageRecruiter(data)
+        .then((result) => commonHelper.response(res, result.rows, 200, "Update image success"))
         .catch((err) => res.send(err));
     } catch (error) {
       console.log(error);
     }
   },
 
-  updateBannerRecruiter: async (req, res) => {
+  updateBannerProfile: async (req, res) => {
     try {
-      const email = req.payload.email;
+      const id = req.params.id;
 
       const result = await cloudinary.uploader.upload(req.file.path);
       const banner_image = result.secure_url;
 
       const {
-        rows: [recruiter],
-      } = await findEmail(email);
+        rows: [users],
+      } = await findId(id);
 
       const data = {
-        id: recruiter.id,
+        id: users.id,
         banner_image,
       };
 
-      if (recruiter.banner_image !== null) {
-        const public_id = recruiter.banner_image.split("/").pop().split(".").shift();
+      if (users.banner_image !== null) {
+        const public_id = users.banner_image.split("/").pop().split(".").shift();
         await cloudinary.uploader.destroy(public_id);
       }
 
-      updateBanner(data)
+      updateBannerRecruiter(data)
         .then((result) => commonHelper.response(res, result.rows, 200, "Update banner success"))
         .catch((err) => res.send(err));
     } catch (error) {
@@ -221,25 +134,20 @@ const recruiterController = {
     }
   },
 
-  deleteRecruiters: async (req, res) => {
+  deleteUsers: async (req, res) => {
     try {
       const email = req.payload.email;
 
       const {
-        rows: [recruiter],
+        rows: [users],
       } = await findEmail(email);
 
-      if (recruiter.image !== null) {
-        const public_id = recruiter.image.split("/").pop().split(".").shift();
+      if (users.image !== null) {
+        const public_id = users.image.split("/").pop().split(".").shift();
         await cloudinary.uploader.destroy(public_id);
       }
 
-      if (recruiter.banner_image !== null) {
-        const public_id = recruiter.banner_image.split("/").pop().split(".").shift();
-        await cloudinary.uploader.destroy(public_id);
-      }
-
-      deleteRecruiter(email)
+      deleteUsersAccount(email)
         .then((result) => commonHelper.response(res, result.rows, 200, "Delete success"))
         .catch((err) => res.send(err));
     } catch (error) {
@@ -248,4 +156,4 @@ const recruiterController = {
   },
 };
 
-module.exports = recruiterController;
+module.exports = recruiterControllers;
