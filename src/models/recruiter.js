d26ed4a02865c40256a86d52131cda08fa2b077d
Hasbi Alwi Kusmana <hasbialwi70@gmail.com>
2023-12-30 20:11:15 +0700

    Update recruiter.js

--- src/models/recruiter.js
+++ src/models/recruiter.js
@@ -1,83 +1,56 @@
 const Pool = require("../config/db");
 
-const findEmail = (email) => {
-  return new Promise((resolve, reject) =>
-    Pool.query(`SELECT * FROM recruiters WHERE email='${email}'`, (error, result) => {
-      if (!error) {
-        resolve(result);
-      } else {
-        reject(error);
-      }
-    })
-  );
-};
-
-const selectAllRecruiters = () => {
-  return Pool.query(`SELECT * FROM recruiters`);
-};
-const selectWorker = (id) => {
-  return new Promise((resolve, reject) => Pool.query(`SELECT * FROM workers WHERE id='${id}'`, (error, result) => (!error ? resolve(result) : reject(error))));
-};
-
-const createUser = (data) => {
-  const { id, name, email, company_name, jobdesk, nohp, password, role } = data;
-  return Pool.query(`INSERT INTO recruiters (id, name, email, company_name,jobdesk,nohp,password,role) VALUES ('${id}', '${name}', '${email}', '${company_name}','${jobdesk}','${nohp}','${password}','${role}')`);
-};
-
-const updaterecruiter = (data) => {
-  const { id, name, company_name, jobdesk, nohp, company_field, workplace, description, instagram, linkedin } = data;
+const selectAllRecruiter = (filter, searchQuery, sortBy, sort, limit, offset) => {
   return Pool.query(
-    `UPDATE recruiters SET name='${name}', company_name='${company_name}', jobdesk='${jobdesk}', nohp='${nohp}', company_field='${company_field}', workplace='${workplace}', description='${description}', instagram='${instagram}', linkedin='${linkedin}' WHERE id='${id}'`
+    `SELECT users.id, users.name, users.email, users.company_name, users.jobdesk, users.nohp, users.company_field, users.workplace, users.description, users.instagram, users.linkedin, users.image, users.banner_image FROM users WHERE users.role='recruiters' AND users.name ILIKE '%${searchQuery}%' ORDER BY ${filter} ${sort} LIMIT ${limit} OFFSET ${offset}`
   );
 };
 
-const updatePhotoUser = (data) => {
+const selectProfileRecruiter = (id) => {
+  return new Promise((resolve, reject) => {
+    Pool.query(
+      `SELECT users.name, users.email, users.company_name, users.jobdesk, users.nohp, users.company_field, users.workplace, users.description, users.instagram, users.linkedin, users.image, users.banner_image FROM users WHERE users.id='${id}'`,
+      (error, result) => {
+        if (!error) {
+          resolve(result);
+        } else {
+          reject(error);
+        }
+      }
+    );
+  });
+};
+
+const updateRecruiter = (data) => {
+  const { id, name, email, nohp, company_name, jobdesk, company_field, workplace, description, instagram, linkedin } = data;
+  return new Promise((resolve, reject) => {
+    Pool.query(
+      `UPDATE users SET name='${name}', email='${email}', nohp='${nohp}', company_name='${company_name}', jobdesk='${jobdesk}', company_field='${company_field}', workplace='${workplace}', description='${description}', instagram='${instagram}', linkedin='${linkedin}' WHERE id='${id}'`,
+      (error, result) => {
+        if (!error) {
+          resolve(result);
+        } else {
+          reject(error);
+        }
+      }
+    );
+  });
+};
+
+const updateImageRecruiter = (data) => {
   const { id, image } = data;
-  return Pool.query(`UPDATE recruiters SET image='${image}' WHERE id='${id}'`);
+  return Pool.query(`UPDATE users SET image='${image}' WHERE id='${id}'`);
 };
 
-const updateBanner = (data) => {
-  const { id, banner_image } = data;
-  return Pool.query(`UPDATE recruiters SET banner_image='${banner_image}' WHERE id='${id}'`);
-};
-
-const countData = () => {
-  return Pool.query(`SELECT COUNT(*) FROM recruiters`);
-};
-
-const findId = (id) => {
-  return new Promise((resolve, reject) =>
-    Pool.query(`SELECT id FROM recruiters WHERE id='${id}'`, (error, result) => {
-      if (!error) {
-        resolve(result);
-      } else {
-        reject(error);
-      }
-    })
-  );
-};
-
-const deleteRecruiter = (email) => {
-  return new Promise((resolve, reject) =>
-    Pool.query(`DELETE FROM recruiters WHERE email='${email}'`, (error, result) => {
-      if (!error) {
-        resolve(result);
-      } else {
-        reject(error);
-      }
-    })
-  );
+const updateBannerRecruiter = (data) => {
+  const { id, image, banner_image } = data;
+  return Pool.query(`UPDATE users SET image='${image}', banner_image='${banner_image}' WHERE id='${id}'`);
 };
 
 module.exports = {
-  findEmail,
-  selectAllRecruiters,
-  createUser,
-  updatePhotoUser,
-  updaterecruiter,
-  findId,
-  countData,
-  selectWorker,
-  updateBanner,
-  deleteRecruiter,
+  selectAllRecruiter,
+  selectProfileRecruiter,
+  updateRecruiter,
+  updateImageRecruiter,
+  updateBannerRecruiter,
 };
