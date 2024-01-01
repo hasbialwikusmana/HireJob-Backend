2c3cf7b3e8f4a2246e01f023d801b11303f24d14
Hasbi Alwi Kusmana <hasbialwi70@gmail.com>
2024-01-02 00:00:14 +0700

    Update recruiter.js

--- src/routes/recruiter.js
+++ src/routes/recruiter.js
@@ -4,8 +4,8 @@ const recruiterControllers = require("../controllers/recruiter");
 const { protect, isRecruiter } = require("../middlewares/auth");
 const { upload, uploadBanner } = require("../middlewares/upload");
 
-router.get("/", protect, recruiterControllers.getAllRecruiter);
-router.get("/:id", protect, isRecruiter, recruiterControllers.getRecruiterById);
+router.get("/", recruiterControllers.getAllRecruiter);
+router.get("/:id", recruiterControllers.getRecruiterById);
 router.put("/profile/:id", protect, isRecruiter, recruiterControllers.updateProfileRecruiter);
 
 router.put("/profile/update-image/:id", protect, upload, isRecruiter, recruiterControllers.updateImageProfile);
