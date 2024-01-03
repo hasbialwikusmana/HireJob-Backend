c7d9f0815b0df6597a2072311e4d76f9c1cb03b7
Hasbi Alwi Kusmana <hasbialwi70@gmail.com>
2024-01-04 00:34:22 +0700

    Update worker.js

--- src/validations/worker.js
+++ src/validations/worker.js
@@ -1,5 +1,41 @@
 const { check } = require("express-validator");
 
+const createWorker = [
+  // name
+  check("name").notEmpty().withMessage("Name cannot be empty"),
+
+  // email
+  check("email").notEmpty().withMessage("Email cannot be empty"),
+  check("email", "Email must be gmail.com").custom((value) => {
+    if (!value.endsWith("@gmail.com")) {
+      throw new Error("Email must be gmail.com");
+    }
+    return true;
+  }),
+
+  // nohp
+  check("nohp").notEmpty().withMessage("Nohp cannot be empty"),
+  check("nohp", "Nohp must be number").custom((value) => {
+    if (isNaN(value)) {
+      throw new Error("Nohp must be number");
+    }
+    return true;
+  }),
+
+  // password
+  check("password").notEmpty().withMessage("Password cannot be empty"),
+  check("password", "Password must be at least 8 characters").isLength({ min: 8 }),
+
+  //role
+  check("role").notEmpty().withMessage("Role cannot be empty"),
+  check("role", "Role must be workers").custom((value) => {
+    if (value !== "workers") {
+      throw new Error("Role must be workers");
+    }
+    return true;
+  }),
+];
+
 const updateWorker = [
   // name
   check("name").notEmpty().withMessage("Name cannot be empty"),
@@ -32,4 +68,4 @@ const updateWorker = [
   check("gitlab").notEmpty().withMessage("Gitlab cannot be empty"),
 ];
 
-module.exports = updateWorker;
+module.exports = { updateWorker, createWorker };
