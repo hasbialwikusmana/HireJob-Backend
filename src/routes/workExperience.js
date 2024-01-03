a1df67b4c85fca1d3155c0080945cb86c9629c93
Hasbi Alwi Kusmana <hasbialwi70@gmail.com>
2024-01-04 00:34:12 +0700

    Update workExperience.js

--- src/routes/workExperience.js
+++ src/routes/workExperience.js
@@ -5,10 +5,14 @@ const { protect, isWorker } = require("../middlewares/auth");
 
 const { getAllWorkExperiences, getDetailWorkExperience, createWorkExperience, updateWorkExperience, deleteWorkExperience } = require("../controllers/workExperience");
 
+// validate
+const { createWorkExperienceValidation, updateWorkExperienceValidation } = require("../validations/workExperience");
+const validation = require("../middlewares/validation");
+
 router.get("/", getAllWorkExperiences);
 router.get("/:id", getDetailWorkExperience);
-router.post("/", protect, isWorker, createWorkExperience);
-router.put("/:id", protect, isWorker, updateWorkExperience);
+router.post("/", protect, isWorker, createWorkExperienceValidation, validation, createWorkExperience);
+router.put("/:id", protect, isWorker, updateWorkExperienceValidation, validation, updateWorkExperience);
 router.delete("/:id", protect, isWorker, deleteWorkExperience);
 
 module.exports = router;
