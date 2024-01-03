1bd88c6a9fb2215dd8ad584d2a9f583aecfe469b
Hasbi Alwi Kusmana <hasbialwi70@gmail.com>
2024-01-04 00:34:05 +0700

    Update skill.js

--- src/routes/skill.js
+++ src/routes/skill.js
@@ -3,11 +3,13 @@ const router = express.Router();
 
 const { protect, isWorker } = require("../middlewares/auth");
 const skillControllers = require("../controllers/skill");
+const createSkillValidation = require("../validations/skill");
+const validation = require("../middlewares/validation");
 
 //Skills router
 router.get("/", skillControllers.getAllSkills);
 router.get("/:id", skillControllers.getDetailSkill);
-router.post("/", protect, isWorker, skillControllers.createSkill);
+router.post("/", protect, isWorker, createSkillValidation, validation, skillControllers.createSkill);
 router.delete("/:id", protect, isWorker, skillControllers.deleteSkill);
 
 module.exports = router;
