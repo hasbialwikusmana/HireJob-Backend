f13964da917e415ee10fb23d6757c83dedf731d5
Hasbi Alwi Kusmana <hasbialwi70@gmail.com>
2024-01-04 00:34:17 +0700

    Update recruiter.js

--- src/validations/recruiter.js
+++ src/validations/recruiter.js
@@ -2,9 +2,13 @@ const { check } = require("express-validator");
 
 const updateRecruiterValidation = [
   check("company_name", "Company name is required").notEmpty(),
+
   check("company_field", "Company field is required").notEmpty(),
+
   check("residence", "Residence is required").notEmpty(),
+
   check("description", "Description is required").notEmpty(),
+
   check("email", "Email is required").notEmpty(),
   check("email", "Email must be gmail.com").custom((value) => {
     if (!value.endsWith("@gmail.com")) {
@@ -12,8 +16,11 @@ const updateRecruiterValidation = [
     }
     return true;
   }),
+
   check("nohp", "No HP is required").notEmpty(),
+
   check("instagram", "Instagram is required").notEmpty(),
+
   check("linkedin", "Linkedin is required").notEmpty(),
 ];
 
