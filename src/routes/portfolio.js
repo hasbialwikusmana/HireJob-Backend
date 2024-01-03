b45bf1b9b89d246c11e86124d58778fec7c7b086
Hasbi Alwi Kusmana <hasbialwi70@gmail.com>
2024-01-04 00:34:03 +0700

    Update portfolio.js

--- src/routes/portfolio.js
+++ src/routes/portfolio.js
@@ -6,10 +6,14 @@ const { protect, isWorker } = require("../middlewares/auth");
 
 const { getAllPortfolios, getDetailPortfolio, createPortfolio, updatePortfolio, deletePortfolio } = require("../controllers/portfolio");
 
+// validate
+const { createPortfolioValidation, updatePortfolioValidation } = require("../validations/portfolio");
+const validation = require("../middlewares/validation");
+
 router.get("/", getAllPortfolios);
 router.get("/:id", getDetailPortfolio);
-router.post("/", protect, isWorker, upload, createPortfolio);
-router.put("/:id", protect, isWorker, upload, updatePortfolio);
+router.post("/", protect, isWorker, upload, createPortfolioValidation, validation, createPortfolio);
+router.put("/:id", protect, isWorker, upload, updatePortfolioValidation, validation, updatePortfolio);
 router.delete("/:id", protect, isWorker, deletePortfolio);
 
 module.exports = router;
