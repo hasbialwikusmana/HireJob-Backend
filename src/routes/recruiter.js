9c8c11e8d724eaec4e2c85d43b6d3afacde1ae1f
Hasbi Alwi Kusmana <hasbialwi70@gmail.com>
2024-01-04 10:45:22 +0700

    Update recruiter.js

--- src/routes/recruiter.js
+++ src/routes/recruiter.js
@@ -3,16 +3,16 @@ const router = express.Router();
 const recruiterControllers = require("../controllers/recruiter");
 const { protect, isRecruiter } = require("../middlewares/auth");
 const { upload, uploadBanner } = require("../middlewares/upload");
-const updateRecruiterValidation = require("../validations/recruiter");
+const { updateRecruiterValidation, updateImageValidation, updateBannerValidation } = require("../validations/recruiter");
 const validation = require("../middlewares/validation");
 
 router.get("/", recruiterControllers.getAllRecruiter);
 router.get("/:id", recruiterControllers.getRecruiterById);
 router.put("/profile/:id", protect, isRecruiter, updateRecruiterValidation, validation, recruiterControllers.updateProfileRecruiter);
 
-router.put("/profile/update-image/:id", protect, upload, isRecruiter, recruiterControllers.updateImageProfile);
+router.put("/profile/update-image/:id", protect, upload, isRecruiter, updateImageValidation, validation, recruiterControllers.updateImageProfile);
 
-router.put("/profile/update-banner/:id", protect, isRecruiter, uploadBanner, recruiterControllers.updateBannerProfile);
+router.put("/profile/update-banner/:id", protect, isRecruiter, uploadBanner, updateBannerValidation, validation, recruiterControllers.updateBannerProfile);
 
 router.delete("/profile/delete/:id", protect, isRecruiter, recruiterControllers.deleteUsers);
 
