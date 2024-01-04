c6adbb31a99f296a56e8f881ca0b5847061002fa
Hasbi Alwi Kusmana <hasbialwi70@gmail.com>
2024-01-04 10:45:17 +0700

    Update recruiter.js

--- src/validations/recruiter.js
+++ src/validations/recruiter.js
@@ -24,4 +24,50 @@ const updateRecruiterValidation = [
   check("linkedin", "Linkedin is required").notEmpty(),
 ];
 
-module.exports = updateRecruiterValidation;
+const updateImageValidation = [
+  check("image").custom((value, { req }) => {
+    if (!req.file) {
+      throw new Error("Image is required");
+    }
+
+    const allowedExtensions = ["jpg", "jpeg", "png"]; // Sesuaikan dengan ekstensi gambar yang diizinkan
+    const maxFileSize = 2 * 1024 * 1024; // 2MB
+
+    const fileExtension = req.file.originalname.split(".").pop().toLowerCase();
+
+    if (!allowedExtensions.includes(fileExtension)) {
+      throw new Error("Only JPG, JPEG, PNG, and GIF files are allowed");
+    }
+
+    if (req.file.size > maxFileSize) {
+      throw new Error("Image size must be less than 2MB");
+    }
+
+    return true;
+  }),
+];
+
+const updateBannerValidation = [
+  check("banner_image").custom((value, { req }) => {
+    if (!req.file) {
+      throw new Error("Banner image is required");
+    }
+
+    const allowedExtensions = ["jpg", "jpeg", "png"]; // Sesuaikan dengan ekstensi gambar yang diizinkan
+    const maxFileSize = 2 * 1024 * 1024; // 2MB
+
+    const fileExtension = req.file.originalname.split(".").pop().toLowerCase();
+
+    if (!allowedExtensions.includes(fileExtension)) {
+      throw new Error("Only JPG, JPEG, PNG, and GIF files are allowed");
+    }
+
+    if (req.file.size > maxFileSize) {
+      throw new Error("Banner image size must be less than 2MB");
+    }
+
+    return true;
+  }),
+];
+
+module.exports = { updateRecruiterValidation, updateImageValidation, updateBannerValidation };
