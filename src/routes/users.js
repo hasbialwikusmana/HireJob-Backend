b432d137e8b2dff1d5d8916de6d72834f43f8ea7
Hasbi Alwi Kusmana <hasbialwi70@gmail.com>
2024-01-05 00:54:44 +0700

    add validasi

--- src/routes/users.js
+++ src/routes/users.js
@@ -2,9 +2,11 @@ const express = require("express");
 const router = express.Router();
 const usersControllers = require("../controllers/users");
 const { protect } = require("../middlewares/auth");
+const { registerValidation, loginValidation } = require("../validations/users");
+const validation = require("../middlewares/validation");
 
-router.post("/register", usersControllers.register);
-router.post("/login", usersControllers.login);
+router.post("/register", registerValidation, validation, usersControllers.register);
+router.post("/login", loginValidation, validation, usersControllers.login);
 router.get("/profile", protect, usersControllers.getProfile);
 
 module.exports = router;
